71f8c1fb37d4ece33fd6b201c8d3474db20df0f2
Aman Kumar Prasad <amankumarprasad72@gmail.com>
2023-08-15 23:02:36 +0530

    new modules

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { BrowserRouter,Navigate,Routes,Route } from "react-router-dom";
+import HomePage from "scenes/HomePage/Home";
+import LoginPage from "scenes/loginPage/Login";
+import NavBar from "scenes/navbar/NavBar";
+import ProfilePage from "scenes/profilePage/Profile";
 
 const App = () => {
-  return <div>App</div>;
+  return <div  className="app">
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<LoginPage/>}/>
+      <Route path="/home" element={<HomePage/>}/>
+      <Route path="/profile/:userID" element={<ProfilePage/>}/>
+
+    </Routes>
+    </BrowserRouter>
+  </div>;
 };
 
 export default App;
